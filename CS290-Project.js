079cb009df48889774b11ec3f4987e2fb856fa41
ijbonn <bonnerij@gmail.com>
2021-05-23 17:02:04 -0600

    Field creation and details

--- CS290-Project.js
+++ CS290-Project.js
@@ -21,13 +21,14 @@ app.set('port', 4494);
 // Start home page or ask for new session if none is active        
 app.get('/',function(req,res,next){
   var context = {};
-  if(!req.session.name){
+  if(!req.session.userName){
     res.render('newSession', context);
     return;
   }
-  context.name = req.session.name;
+  context.userName = req.session.userName;
   context.fieldCount = req.session.fieldInfo.length || 0;
   context.fieldInfo = req.session.fieldInfo || [];
+  console.log(context.fieldInfo);
   res.render('home', context);
 });
 
@@ -35,25 +36,15 @@ app.post('/',function(req,res){
   var context = {};
 
   if(req.body['New User']){
-    req.session.name = req.body.name;
-    req.session.fieldInfo = [];
-    req.session.fieldId = 0;
+    req.session.userName = req.body.userName;
+    var defaultFields = require('./public/json/defaultFields.json');
+    req.session.fieldInfo = defaultFields;
+    req.session.fieldId = 3;
   }
 
-  if(!req.session.name){
+  if(!req.session.userName){
     res.render('newSession', context);
     return;
-  }
-
-  if(req.body['Add Field']){
-    req.session.fieldInfo.push({"name":req.body.name, "id":req.session.fildId});
-    req.session.fieldId++;
-  }
-
-  if(req.body['Delete Field']){
-    req.session.fieldInfo.filter(function(e){
-      return e.id != req.body.id;
-    })
   }
 
   if(req.body['End Session']){
@@ -62,36 +53,87 @@ app.post('/',function(req,res){
     return;
   }
 
-  context.name = req.session.name;
-  context.fielCount = req.session.fieldInfo.length;
+  context.userName = req.session.userName;
+  context.fieldCount = req.session.fieldInfo.length;
   context.fieldInfo = req.session.fieldInfo;
   res.render('home',context);
 });
 
+function fetchField(fArray,fId){
+  const foundField = fArray.filter(field => field.id == fId);
+  var singleField = new Object();
+  singleField = foundField[0];
+  return singleField;
+}
+
 app.get('/create-field',function(req,res){
   var context = {};
-  if(!req.session.name){
+  if(!req.session.userName){
     res.render('newSession', context);
     return;
   }
+  context.userName = req.session.userName;
   res.render('create-field', context);
+});
+
+app.post('/create-field',function(req,res){
+  var context = {};
+  
+  let [month, date, year] = new Date().toLocaleDateString("en-US").split("/");
+  currentDate = year + '-' + month + '-' + date;
+
+  if(req.body['Add Field']){
+    req.session.fieldInfo.push({"id":req.session.fieldId, "fName":req.body.fieldName, "fState":req.body.fieldState, 
+      "fCity":req.body.fieldCity, "fCrop":req.body.fieldCrop, "fAcres":req.body.fieldAcres, "fPDate":req.body.fieldPlantingDate,
+      "cUser":req.session.userName, "cDate":currentDate});
+    //const newField = req.session.fieldInfo.filter(field => field.id == req.session.fieldId);
+    //console.log(newField);
+    var newField = fetchField(req.session.fieldInfo,req.session.fieldId)
+    req.session.fieldId++;
+    context.fieldInfo = newField;
+    context.userName = req.session.userName;
+    res.render('field-details', context);
+    return;    
+  }
+  context.userName = req.session.userName;
+  res.render('create-field',context);
 });
 
 app.get('/browse-fields',function(req,res){
   var context = {};
-  if(!req.session.name){
+  if(!req.session.userName){
     res.render('newSession', context);
     return;
   }
+  context.userName = req.session.userName;
+  context.fieldInfo = req.session.fieldInfo;
   res.render('browse-fields', context);
+});
+
+app.post('/browse-fields',function(req,res){
+  var context = {};
+  
+  if(req.body['Details']){
+    //const targetField = req.session.fieldInfo.filter(field => field.id == req.body.id);
+    var targetField = fetchField(req.session.fieldInfo,req.body.id)
+    //context.fieldInfo = targetField;
+    context.fieldInfo = targetField;
+    context.userName = req.session.userName;
+    res.render('field-details', context);
+    return;    
+  }
+  context.fieldInfo = req.session.fieldInfo;
+  context.userName = req.session.userName;
+  res.render('browse-field',context);
 });
 
 app.get('/about',function(req,res){
   var context = {};
-  if(!req.session.name){
+  if(!req.session.userName){
     res.render('newSession', context);
     return;
   }
+  context.userName = req.session.userName;
   res.render('about', context);
 });
 
